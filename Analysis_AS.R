f1e621ea990b7ea559180f5a77a5beccc10fe35c
MQuenu <85265019+MQuenu@users.noreply.github.com>
2023-10-13 14:43:15 +0200

    Update Analysis_AS.R

--- Analysis_AS.R
+++ Analysis_AS.R
@@ -3,19 +3,21 @@ library(RColorBrewer)
 library(dplyr)
 library(entropy)
 library(gridExtra)
+library(emmeans)
+library(lme4)
 
 setwd("~/DBLa/R_analyses")
 
 rm(list = ls())
 
-## format the input table, only do it once
+#### format the input table, only do it once ####
 
 ASprocessed_df <- read.csv("ASformatted_table.csv") %>%
-  select(-X) %>%
   mutate(NTS = substr(Domain1, start = 1, stop = 4),
          Read_count = as.numeric(Read_count),
          infection_group = substr(sample, start = 1, stop = 2),
-         host = substr(sample, start = 1, stop = 4)) %>% 
+         host = substr(sample, start = 1, stop = 4),
+         timepoint = ifelse(nchar(sample) == 7, substr(sample, start = 6, stop = 7), NA)) %>% 
   group_by(sample) %>%
   mutate(total_reads = sum(Read_count)) %>%
   ungroup() %>%
@@ -30,11 +32,24 @@ ASprocessed_df <- read.csv("ASformatted_table.csv") %>%
   mutate(recurrence_step1 = ifelse(n() > 1, "recurrent", "single")) %>%
   mutate(recurrence = ifelse(recurrence_step1 == 'recurrent' & Dominant_tags == 'dominant', 'recurrent', 'single')) %>%
   ungroup() %>%
-  select(-recurrence_step1)
-  
-write.csv(ASprocessed_df, "ASformatted_table.csv")
+  select(-recurrence_step1) %>%
+  select(-X)
 
-#### Plots of read counts per sample
+parasitaemia_df <- read.csv("parasitaemia.csv") %>%
+  rename(sample = Alias_updated,
+         parasitaemia = Venous.parasitaemia) %>%
+  select(-seq)
+
+Age_df <- read.csv("host_age.csv") %>%
+  rename(sample = Alias_updated)
+
+updated_df <- left_join(ASprocessed_df, parasitaemia_df, by = "sample")
+updated_df <- left_join(ASprocessed_df, Age_df, by = "sample")
+
+write.csv(updated_df, "ASformatted_table.csv")
+
+
+#### Barplots of read counts per sample ####
 
 ## DC01 
 
@@ -333,7 +348,29 @@ plot_WA3 = ggplot(WA3_dataframe, aes(x = DBL_tag, y = normalized_reads, fill = N
 
 plot_WA3
 
-### Histogram of DBLa expressions
+#### Piecharts plots ####
+
+AS_dataframe <- read.csv("ASformatted_table.csv") 
+
+my_palette <- c("#FFC20A", "#0C7BDC","#3498DB")
+
+plot_DC01 = ggplot(AS_dataframe, aes(x = "", y = normalized_reads, fill = NTS)) +
+  geom_col() +
+  coord_polar(theta = "y") +
+  labs(title = NULL)+
+  facet_wrap(~ sample) +
+  scale_fill_manual(values = my_palette) +
+  theme(axis.text = element_blank(),
+        axis.ticks = element_blank(),
+        axis.title = element_blank(),
+        panel.grid = element_blank(),
+        panel.background = element_rect(fill = "#FFFFFF"),
+        plot.background = element_rect(fill = "#FFFFFF"),
+        legend.background = element_rect(fill = "#FFFFFF")) 
+
+plot_DC01
+
+### Histogram of DBLa expressions ####
 
 df <- read.csv("ASformatted_table.csv", header = TRUE) 
 
@@ -357,41 +394,181 @@ count_dataframe <- raw_dataframe %>%
 
 # look through count_dataframe interactively
 
-## Entropy 
+##### Parasitaemia plots ######
+
+dataframe <- read.csv("ASformatted_table.csv", header = TRUE) %>%
+  filter(infection_group == 'DC') %>%
+  group_by(timepoint) 
+
+parasitaemia_dataframe_timepoints_data <- aggregate(parasitaemia ~ sample + infection_group + host + timepoint, data = dataframe, FUN = mean) %>%
+  group_by(timepoint) %>%
+  mutate(median_parasitaemia = median(parasitaemia)) %>%
+  ungroup() %>%
+  arrange(sample)
+    
+timepoints_parasitaemia <- ggplot(parasitaemia_dataframe_timepoints_data) +
+  geom_point(aes(x = timepoint, y = parasitaemia, group = host, color = host), size = 2) + 
+  geom_line(aes(x = timepoint, y = parasitaemia, group = host, color = host), linetype = 2) +
+  scale_y_continuous(trans='log10') +
+  theme_classic()
+
+timepoints_parasitaemia <- ggplot(parasitaemia_dataframe_timepoints_data) +
+  geom_point(aes(x = timepoint, y = parasitaemia, group = host, color = host), alpha = 0.5, size = 0.5) + 
+  geom_line(aes(x = timepoint, y = parasitaemia, group = host, color = host), linetype = 2, alpha = 0.5) +
+  geom_point(aes(x = timepoint, y = median_parasitaemia), size = 2.5) + 
+  geom_path(aes(x = timepoint, y = median_parasitaemia), color = "black", size = 1) +
+  scale_y_continuous(trans='log10') +
+  ylab("parasitaemia (p/ul)") +
+  theme_classic()
+
+timepoints_parasitaemia
+
+##### Entropy analyses ##### 
 
 rm(list = ls())
 
 dataframe <- read.csv("ASformatted_table.csv", header = TRUE) 
 
-entropy_dataframe <- aggregate(entropy ~ sample + infection_group, data = dataframe, FUN = mean)
+entropy_dataframe_all_data <- aggregate(entropy ~ sample + infection_group + host + parasitaemia, data = dataframe, FUN = mean) %>%
+  arrange(sample)
 
-plot_entropy <- ggplot(entropy_dataframe, aes(x = infection_group, y = entropy, fill = infection_group)) +
+entropy_dataframe_clonality_data <- aggregate(entropy ~ sample + infection_group + host + clonality, data = dataframe, FUN = mean) %>%
+  arrange(sample)
+
+hist_entropy <- ggplot(entropy_dataframe_all_data, aes(x = entropy)) +
+  geom_histogram(binwidth = 0.25, color="black", fill="gray") + 
+  theme_classic()
+
+hist_entropy
+
+boxplot_entropy <- ggplot(entropy_dataframe_all_data, aes(x = infection_group, y = entropy)) +
+  geom_boxplot() +
+  geom_jitter() +
+  theme_classic()
+
+boxplot_entropy
+
+barplot_entropy <- ggplot(entropy_dataframe_all_data, aes(x = sample, y = entropy)) +
+  geom_bar(stat = "identity", na.rm = FALSE) + 
+  theme_classic() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) 
+
+barplot_entropy
+
+boxplot_entropy <- ggplot(entropy_dataframe, aes(x = clonality, y = entropy)) +
   geom_boxplot() +
   geom_jitter() +
   scale_fill_brewer() +
   theme_classic()
 
-plot_entropy
+boxplot_entropy
 
-### glm between the three infection groups
+#### Timepoints data
 
-null_model <- glm(entropy ~ 1, data = entropy_dataframe)
-linear_model <- glm(entropy ~ infection_group, data = entropy_dataframe)
+entropy_dataframe_timepoint_data <- aggregate(entropy ~ sample + infection_group + host + timepoint, data = dataframe, FUN = mean) %>%
+  group_by(timepoint) %>%
+  mutate(mean_entropy = mean(entropy))%>%
+  ungroup %>%
+  arrange(sample) 
 
-lrtest <- anova(null_model, linear_model)
+timepoints_entropy <- ggplot(entropy_dataframe_timepoint_data, aes(x = timepoint, y = entropy, group = host, color = host)) +
+  geom_point(size = 2) + 
+  geom_line(linetype = 2) +
+  scale_color_viridis_b( palette = 'Cividis') +
+  theme_classic()
+
+timepoints_entropy <- ggplot(entropy_dataframe_timepoint_data) +
+  geom_point(aes(x = timepoint, y = entropy, group = host, color = host), size = 1, alpha = 0.5) + 
+  geom_line(aes(x = timepoint, y = entropy, group = host, color = host),linetype = 2, alpha = 0.5) +
+  geom_point(aes(x = timepoint, y = mean_entropy), size = 2, color = "black") + 
+  geom_line(aes(x = timepoint, y = mean_entropy),linetype = 2, color = "black") +
+  theme_classic()
+
+timepoints_entropy
+
+## correlation with parasitaemia
+
+plot_corParasitaemia <- ggplot(data = entropy_dataframe_all_data, aes(x = entropy, y = parasitaemia)) +
+  geom_point(size = 2) + 
+  scale_y_log10() +
+  ylab("parasitaemia (p/ul)") +
+  theme_classic()
+
+plot_corParasitaemia
+
+# fitting glm
+
+initial_values <- c(coef1 = 0, coef2 = 0)
+
+glm_model <- glm(formula = entropy ~ parasitaemia, data = entropy_dataframe_all_data, family = gaussian(link = "log"), start = initial_values)
+glm_model_bis <- glm(formula = entropy ~ parasitaemia, data = entropy_dataframe_all_data)
+
+summary(glm_model)
+summary(glm_model_bis)
+
+####### glms and mixed effect models #####
+
+## per infection group
+
+#simple glm
+
+null_model <- glm(entropy ~ 1, data = entropy_dataframe_all_data)
+full_model <- glm(entropy ~ infection_group, data = entropy_dataframe_all_data)
+
+lrtest <- anova(null_model, full_model)
+
+print(lrtest)
+
 teststat <- lrtest[2, "Deviance"]
 pvalue <- pchisq(teststat, df = 1, lower.tail = FALSE)
 
 pvalue
 
+em <- emmeans(full_model, "infection_group")
+contrast(em, "pairwise", adjust = "Tukey")
+
+## per clonality
+
+null_model <- glm(entropy ~ 1, data = entropy_dataframe)
+full_model <- glm(entropy ~ clonality, data = entropy_dataframe)
+
+lrtest <- anova(null_model, linear_model)
+
+print(lrtest)
+teststat <- lrtest[2, "Deviance"]
+pvalue <- pchisq(teststat, df = 1, lower.tail = FALSE)
+
+pvalue
+
+## per timepoint
+
+### using simple glm
+
+null_model <- glm(entropy ~ 1, data = entropy_dataframe_timepoint_data)
+full_model <- glm(entropy ~ timepoint, data = entropy_dataframe_timepoint_data)
+
+lrtest <- anova(null_model, full_model)
+
+print(lrtest)
+teststat <- lrtest[2, "Deviance"]
+pvalue <- pchisq(teststat, df = 1, lower.tail = FALSE)
+
+pvalue
+
+### using mixed models
+null_model <- lmer(entropy ~  (1 | host), data = entropy_dataframe_timepoint_data)
+linear_model <- lmer(entropy ~ timepoint + (1 | host), data = entropy_dataframe_timepoint_data)
+
+lrtest <- anova(null_model, linear_model)
+
+print(lrtest)
+
 ### Looking for tags that would be expressed twice in the same sample (false clustering)
 
-setwd("~/DBLa/R_analyses/false_clustering")
-
-doubled_dataframe <- read.csv("ASformatted_table.txt",header = TRUE) %>%
-  mutate(sample = substr(Sample_ID, start = 1, stop = 7)) %>%
+doubled_dataframe <- read.csv("ASformatted_table.csv",header = TRUE) %>%
+  mutate(sample = substr(sample, start = 1, stop = 7)) %>%
   group_by(sample) %>%
-  group_by(DBL_tag, add = TRUE) %>%
+  group_by(DBL_tag, .add = TRUE) %>%
   mutate(double_tag = ifelse(n() > 1, "doubled", "single")) %>%
   ungroup() %>%
   filter(Read_count > 1)
@@ -400,8 +577,13 @@ doubled_dataframe <- read.csv("ASformatted_table.txt",header = TRUE) %>%
 
 count_double_dataframe <- doubled_dataframe %>%
   count(double_tag)
+#### Counting the domains and comparative analysis ####
 
-### Looking at Domain composition of expressed var genes
+dataframe <- read.csv("ASformatted_table.csv")
+
+
+
+### Looking at Domain composition of expressed var genes ####
 
 ### functions
 
@@ -413,7 +595,7 @@ for (column in colnames(entry_dataframe)) {
   if (domain == "Domain"){
     summary_df <- entry_dataframe %>%
       group_by(.data[[column]]) %>%
-      summarize(total_quantity = sum(Read_count)) 
+      summarize(total_quantity = sum(normalized_reads)) 
     colnames(summary_df)[1] <- "Expressed_Domain"
     if (exists("combined_data")) {
             combined_data <- full_join(combined_data, summary_df, by = "Expressed_Domain") %>%
@@ -448,38 +630,25 @@ plot_domains <- function(entry_domain_dataframe) {
   return(plot)
 }
 
-### DC01
 
-DC01_m1_dataframe <- read.csv("ASformatted_table.txt", header = TRUE) %>%
-  filter(sample == "DC01_m1")
+### differences in the domain expression of recurrent vs dominant var genes (archives) 
 
-DC01_m3_dataframe <- read.csv("ASformatted_table.txt", header = TRUE) %>%
-  filter(sample == "DC01_m3")
+recurrent_dataframe <- read.csv("ASformatted_table.csv", header = TRUE) %>%
+  filter(recurrence == "recurrent")
 
-DC01_m5_dataframe <- read.csv("ASformatted_table.txt", header = TRUE) %>%
-  filter(sample == "DC01_m5")
+recurrent_tags_domains <- Extract_expression_domain(recurrent_dataframe) %>%
+  mutate(ID = "recurrent")
 
-DC01_m6_dataframe <- read.csv("ASformatted_table.txt", header = TRUE) %>%
-  filter(sample == "DC01_m6")
+plot_recurrent <- plot_domains(recurrent_tags_domains)
 
-DC01_m1_domains <- Extract_expression_domain(DC01_m1_dataframe) %>%
-  mutate(ID = "DC01_m1")
+plot_recurrent
 
-DC01_m3_domains <- Extract_expression_domain(DC01_m3_dataframe) %>%
-  mutate(ID = "DC01_m3")
+dominant_single_dataframe <- read.csv("ASformatted_table.csv", header = TRUE) %>%
+  filter(recurrence == "single" & Dominant_tags == "dominant")
 
-DC01_m5_domains <- Extract_expression_domain(DC01_m5_dataframe) %>%
-  mutate(ID = "DC01_m5")
+dominant_single_domains <- Extract_expression_domain(dominant_single_dataframe) %>%
+  mutate(ID = "single_dominant")
 
-DC01_m6_domains <- Extract_expression_domain(DC01_m6_dataframe) %>%
-  mutate(ID = "DC01_m6")
+plot_dominant_single <- plot_domains(dominant_single_domains)
 
-plot_m1 <- plot_domains(DC01_m1_domains)
-
-plot_m3 <- plot_domains(DC01_m3_domains)
-
-plot_m5 <- plot_domains(DC01_m5_domains)
-
-plot_m6 <- plot_domains(DC01_m6_domains)
-
-grid.arrange(plot_m1, plot_m3, plot_m5, plot_m6, ncol=4, nrow = 1)
+plot_dominant_single
